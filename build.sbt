163b2f2492a1e93bff5e357816c1636d2bb71646
John Henrique Teixeira de Godoi <jgodoi1@its.jnj.com>
2018-03-06 20:12:09 -0300

    reading a xlsx

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.10.4"
 
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.1.1"
+  "org.apache.spark" %% "spark-core" % "2.1.1",
+  "org.apache.spark" %% "spark-sql" % "2.1.1",
+  "com.crealytics" %% "spark-excel" % "0.8.3"
 )
         
--- src/main/scala/Starter.scala
+++ src/main/scala/Starter.scala
@@ -1,3 +1,5 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -5,13 +7,33 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object Starter extends App {
 
-  val conf = new SparkConf().setAppName("workdCount").setMaster("local")
+  val conf = new SparkConf().setAppName("wordCount").setMaster("local")
   val sc = new SparkContext(conf)
-    sc.setLogLevel("OFF")
+  sc.setLogLevel("OFF")
   val nums = sc.parallelize(List(1,2,3,4))
   val squared = nums.map(x=>x*x)
+  private val cached: RDD[Int] = squared.cache()
 
-  println(squared.collect().mkString(","))
+  println(cached.sum())
+  println(cached.collect().mkString(","))
+
+
+
   val file = sc.textFile("build.sbt")
   println(file.filter(x => !x.isEmpty).filter(x => x.contains(":=")).map(x=>x.split(":=")(1).trim).collect().mkString(","))
+
+  private val sqlContext = new SQLContext(sc)
+
+  def readExcel(file: String): DataFrame = sqlContext.read
+    .format("com.crealytics.spark.excel")
+    .option("location", file)
+    .option("useHeader", "true")
+    .option("treatEmptyValuesAsNulls", "true")
+    .option("inferSchema", "false")
+    .option("addColorColumns", "False")
+    .load()
+
+  val data = readExcel("path.xlsx")
+  data.show(false)
 }
+
