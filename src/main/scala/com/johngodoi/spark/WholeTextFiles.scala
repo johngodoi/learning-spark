92c3631d2a43d0ad4aed4d093e4f51381a25f5ed
John Henrique Teixeira de Godoi <jgodoi1@its.jnj.com>
2018-10-12 17:47:04 -0300

    * spark core exercises

---
+++ src/main/scala/com/johngodoi/spark/WholeTextFiles.scala
@@ -0,0 +1,15 @@
+package com.johngodoi.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WholeTextFiles extends App {
+  val conf = new SparkConf().setAppName("initial").setMaster("local")
+  val sc = new SparkContext(conf)
+  sc.setLogLevel("OFF")
+  val wholeTextFiles = sc.wholeTextFiles("./src/main/resources/lego/colors*")
+  //file:path,first line
+  //another lines
+  wholeTextFiles.foreach(file => println(s"${file._1},${file._2}"))
+  sc.stop()
+
+}
